c736dbc5f8ef5dbe679e847d1a4b5c874853de0a
Utkarsh Singh <utkarzz1705@gmail.com>
2025-07-24 14:02:48 +0530

    Initial commit: Add Tic Tac Toe web app

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tic Tac Toe Web App
+
+A responsive Tic Tac Toe game made using *HTML, **CSS, and **JavaScript*. This project offers a clean UI, win detection, and a playful user experience.
+
+## Features
+
+- Interactive 3x3 Tic Tac Toe board
+- Real-time win/lose detection and display
+- Reset functionality for new games
+- Responsive web design 
+- Friendly visual feedback with notification and images
+- 2. Open index.html in your browser.
+
+## File Structure
+
+| File/Folder    | Description                       |
+|----------------|-----------------------------------|
+| index.html     | Main HTML file                    |
+| style.css      | App styling                       |
+| script.js      | Game logic and interactivity      |
+| README.md      | Project overview and instructions |
---
+++ script.js
@@ -0,0 +1,72 @@
+console.log("Welcome to Tic Tac Toe");
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameoverAudio = new Audio("gameover.mp3");
+let turn = "X";
+let gameover = false;
+
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
+
+}
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if (
+            boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[1]].innerText === boxtext[e[2]].innerText &&
+            boxtext[e[0]].innerText !== ''
+        ) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
+            gameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+        }
+    });
+}
+
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(Element => {
+    let boxtest = Element.querySelector('.boxtext');
+    Element.addEventListener('click', () => {
+        if (boxtest.innerText === '') {
+            boxtest.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!gameover) {
+
+
+                document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+            }
+        }
+    })
+})
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(Element =>{
+        Element.innerText = ""
+
+    });
+    turn = "X"
+     gameover = false
+
+
+                document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+
+            
+
+
+})
+
